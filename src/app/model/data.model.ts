8a185f5ef05d7b05e386f6a2ada9005e8153922f
Luca Pierini <luca.pierini@blumade.it>
2020-08-13 22:15:59 +0200

    class -> interface

--- src/app/model/data.model.ts
+++ src/app/model/data.model.ts
@@ -1,12 +1,10 @@
-import {User} from './user.model';
+import { User } from './user.model';
 
-export class DataRS {
-    constructor(
-        public page: number,
-        public per_page: number,
-        public total: number,
-        public total_pages: number,
-        public data: User[],
-        public ad: any[]
-    ) {}
+export interface DataRS {
+    page: number,
+    per_page: number,
+    total: number,
+    total_pages: number,
+    data: User[],
+    ad: any[]
 }
